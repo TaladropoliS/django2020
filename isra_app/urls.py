6a8dca25765a24099550bfa127769ab0e915a33a
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2021-01-29 20:38:44 -0300

    se agrega redirect en isra_app

--- isra_app/urls.py
+++ isra_app/urls.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 from django.urls import path     
 from . import views
 urlpatterns = [
     path('', views.index),	   
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.palma)
->>>>>>> e72cdbaa4156bff28ff210185858df6f9206200c
 ]
