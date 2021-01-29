38198218446ecc4b38edaec1fe543d940847316c
Diego Labrin Curin <di3golabrin@gmail.com>
2021-01-29 19:43:42 -0300

    diego labrin

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
+from django import urls
 from django.urls import path
 
 urlpatterns = [
+    path('nueva_app1', include('nueva_app001.urls')
     #    path('admin/', admin.site.urls),
 ]
