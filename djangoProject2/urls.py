487628294c960a24f676809d321767b2a1af5fe3
israelpalma <ipalm001@gmail.com>
2021-01-29 20:19:55 -0300

    se corrige url

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -18,18 +18,8 @@ from django import urls
 from django.urls import path, include
 
 urlpatterns = [
-
-    #    path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('blog/', include('camila_riquelme.urls'))
-=======
-    #path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('nueva_app1', include('nueva_app001.urls')
->>>>>>> 257762343cb13630f74bd3825691968193246bab
-=======
-    path('diego_esparza', include('diego_esparza.urls')),
     path('blog/', include('camila_riquelme.urls')),
-    path('blog/', include('blog.urls'))
->>>>>>> c9a283ea276a7556ad37dbe89a610a271e632e8c
+    path('blog/', include('blog.urls')),
+    path('nueva_app1', include('nueva_app001.urls')),
+    path('diego_esparza', include('diego_esparza.urls'))
 ]
