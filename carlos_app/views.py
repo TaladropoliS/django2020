87c38cc555ebeeb2eb9523c4207d59e7da091c63
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2021-01-29 21:02:58 -0300

    se agrega templates en carlos_app

--- carlos_app/views.py
+++ carlos_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return render(request,"index.html")
 
 # Create your views here.
