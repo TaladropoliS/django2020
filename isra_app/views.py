aa7d121aa6695459b9142e6900331d7d7347f681
israelpalma <ipalm001@gmail.com>
2021-01-29 21:17:21 -0300

    Validacion y HTML

--- isra_app/views.py
+++ isra_app/views.py
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import User
-=======
-from django.shortcuts import render, HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .models import User
-
-
-def index(request):
-    return HttpResponse('hola')
-
->>>>>>> daa260eb2d3df252f6c93a6417e34cae72d69a31
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
